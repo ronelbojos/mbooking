a4c901a20e08aabf48150b989c6159ef2e99e8ca
Mario Bojos <ronelbojos@gmail.com>
2021-05-18 00:59:06 +0800

    Add more seed records

--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -19,8 +19,71 @@ class BookingSeeder extends Seeder
             ->insert([
                 'user_id' => '1',
                 'room_id' => '1',
-                'date_start' => Carbon::now()->next('Tuesday')->next('8:00'),
-                'date_end' => Carbon::now()->next('Tuesday')->next('10:00'),
+                'date_start' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 08:00'),
+                'date_end' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 08:30'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        DB::table('bookings')
+            ->insert([
+                'user_id' => '2',
+                'room_id' => '2',
+                'date_start' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 08:00'),
+                'date_end' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 08:30'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        DB::table('bookings')
+            ->insert([
+                'user_id' => '1',
+                'room_id' => '3',
+                'date_start' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 09:00'),
+                'date_end' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 09:30'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        DB::table('bookings')
+            ->insert([
+                'user_id' => '2',
+                'room_id' => '1',
+                'date_start' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 09:00'),
+                'date_end' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 09:30'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        DB::table('bookings')
+            ->insert([
+                'user_id' => '1',
+                'room_id' => '2',
+                'date_start' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 10:00'),
+                'date_end' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 10:30'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        DB::table('bookings')
+            ->insert([
+                'user_id' => '2',
+                'room_id' => '3',
+                'date_start' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 10:00'),
+                'date_end' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 10:30'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        DB::table('bookings')
+            ->insert([
+                'user_id' => '1',
+                'room_id' => '1',
+                'date_start' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 11:00'),
+                'date_end' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 11:30'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        DB::table('bookings')
+            ->insert([
+                'user_id' => '2',
+                'room_id' => '2',
+                'date_start' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 11:00'),
+                'date_end' => Carbon::createFromFormat('Y-m-d H:i', '2021-06-01 11:30'),
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ]);
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -18,9 +19,21 @@ class UserSeeder extends Seeder
     {
         DB::table('users')
             ->insert([
-                'name' => 'mario',
-                'email' => 'mariob@gmail.com',
+                'name' => 'Mario Bojos',
+                'email' => 'mario@gmail.com',
+                'password' => Hash::make('mario'),
+                'remember_token' => Str::random(10),
+                'email_verified_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        DB::table('users')
+            ->insert([
+                'name' => 'Ronel Bojos',
+                'email' => 'ronel@gmail.com',
                 'password' => Hash::make('ronel'),
+                'remember_token' => Str::random(10),
+                'email_verified_at' => Carbon::now(),
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ]);
