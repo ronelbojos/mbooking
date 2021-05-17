a380a96184f483c16115a296e40bb055cb19a5ea
Mario Bojos <ronelbojos@gmail.com>
2021-05-17 21:39:57 +0800

    Setup Sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookingController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('bookings', [BookingController::class, 'index']);
 Route::post('bookings', [BookingController::class, 'store']);
